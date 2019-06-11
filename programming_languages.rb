4a944a306264b749d90f5b3b3b25ccad723eaf3a
The Natural <philip.smalls86@gmail.com>
2019-06-11 01:32:08 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    languages.each do |style, language|
+      language.each do |name, attribute|
+        new_hash[name] ||= attribute
+          new_hash[name][:style] = []
+            new_hash[name][:style] << style
+        end
+    end
+    new_hash[:javascript][:style] << :oo
+    new_hash
 end
+
+
+
+
